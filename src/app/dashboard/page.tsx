6a0dc2cf0543f62ee41d9254dcadbd47103d1ead
Astha <iyandabes1@gmail.com>
2025-10-10 15:08:12 +0700

    add: dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+    return (
+        <div className="p-6 flex flex-1 justify-center items-center">
+            <p className="text-center">Hello World</p>
+        </div>
+    )
+}
