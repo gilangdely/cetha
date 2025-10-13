84fb4e59512e98317ecbac87dfc988654607f660
Astha <iyandabes1@gmail.com>
2025-10-14 00:49:23 +0700

    add: sidebarTriger

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -6,7 +6,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <SidebarProvider className="bg-Background p-2">
       <AppSidebar />
       <div className="flex flex-1 flex-col">
-        <main className="flex flex-1 rounded-2xl bg-white p-1">{children}</main>
+        <main className="flex flex-1 rounded-2xl bg-white p-1">
+          <SidebarTrigger />
+          {children}
+        </main>
       </div>
     </SidebarProvider>
   );
